60b97481a4365097389d438531ae0b4cf46359b3
Roshanbadgujar <hacktecher04@gmail.com>
2025-05-01 12:33:12 +0530

    soory bahen

---
+++ app.js
@@ -0,0 +1,11 @@
+// script.js
+function closeCard() {
+    document.querySelector(".container").innerHTML = `
+      <div style="text-align:center; padding:40px 20px; font-size:18px;">
+        <p>Itni si baat thi... 💌</p>
+        <p>Tu apna khayal rakhna.</p>
+        <div class="emoji bounce" style="margin-top: 20px;">🌸</div>
+      </div>
+    `;
+  }
+  
